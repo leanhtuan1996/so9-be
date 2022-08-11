78b869bf4eb4409c4442b819d496de68ff2d179a
Lê Anh Tuấn <justin.le.1105@gmail.com>
2022-08-12 00:09:31 +0700

    feat(config): integrate typeorm configurations

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,4 +1,11 @@
 export const configuration = () => ({
   NODE_ENV: process.env.NODE_ENV,
   PORT: parseInt(process.env.PORT, 10) || 3000,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: parseInt(process.env.DB_PORT, 10) || 5432,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_DATABASE: process.env.DB_DATABASE,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
 });
