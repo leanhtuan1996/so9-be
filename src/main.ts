17d31c04bc59427e2f4e90d8265c2e30936fde2c
Lê Anh Tuấn <justin.le.1105@gmail.com>
2022-08-12 00:45:12 +0700

    add all exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { MainAppModule } from './app/app.module';
+import { AllExceptionsFilter } from './app/exceptions/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(MainAppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  const configService = app.get<ConfigService>(ConfigService);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter, configService));
   await app.listen(3000);
 }
 bootstrap();
