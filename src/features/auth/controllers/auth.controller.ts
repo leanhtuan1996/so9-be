0d5ab7d241452cfed4c2b1a96edfe34f7c966d5b
Lê Anh Tuấn <justin.le.1105@gmail.com>
2022-08-17 22:01:17 +0700

    add

--- src/features/auth/controllers/auth.controller.ts
+++ src/features/auth/controllers/auth.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Post, UseGuards, Body } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { UserSignInDto, UserSignUpDto } from './dto';
-import { LocalAuthGuard } from './guards/local-auth.guard';
+import { Controller, Post, UseGuards, Body, Version } from '@nestjs/common';
+import { AuthService } from '../services/auth.service';
+import { UserSignInDto, UserSignUpDto } from '../dto';
+import { LocalAuthGuard } from '../guards/local-auth.guard';
 
-@Controller('auth')
+@Controller({
+  path: 'auth',
+})
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
+  @Version('1')
   @Post('sign-in')
   async login(@Body() userLoginDto: UserSignInDto) {
     return this.authService.signInWithEmailPassword(
@@ -16,6 +19,7 @@ export class AuthController {
     );
   }
 
+  @Version('1')
   @Post('sign-up')
   async register(@Body() userLoginDto: UserSignUpDto) {
     return this.authService.signUpWithEmailPassword(
