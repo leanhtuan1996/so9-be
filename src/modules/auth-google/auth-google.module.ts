bf5c5308dfc63ced940b1837e5356214483a5fbc
Lê Anh Tuấn <justin.le.1105@gmail.com>
2022-08-22 22:49:48 +0700

    add auth google

---
+++ src/modules/auth-google/auth-google.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { AuthModule } from '../auth/auth.module';
+import { AuthGoogleController } from './auth-google.controller';
+import { AuthGoogleService } from './auth-google.service';
+
+@Module({
+  imports: [ConfigModule, AuthModule],
+  providers: [AuthGoogleService],
+  exports: [AuthGoogleService],
+  controllers: [AuthGoogleController],
+})
+export class AuthGoogleModule {}
